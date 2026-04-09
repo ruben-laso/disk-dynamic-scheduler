79c99298e850f230d9528f4f29b128d3bdebba56
kulagins <kulagins@hu-berllin.de>
2026-04-09 10:55:24 +0200

    clean lines

--- scripts/clean-lines.sh
+++ scripts/clean-lines.sh
@@ -13,7 +13,7 @@ sed -i 's/\t/ /g' *.txt
 sed -i 's/![^ ]* //' *.txt
 sed -i 's/  / /g' *.txt
 
-sed -i ':a;N;$!ba;s/[[:space:]]*\n[[:space:]]*\(duration_of_algorithm\)/ \1/g' *.txt
+sed -i ':a;N;$!ba;s/[[:space:]]*\n[[:space:]]*\(duration_of_algorithm\)/ \1/g' *.txt #join lines where duration_of_algorithm is at the beginning of the next line
 sed -Ei 's/.*(algo_nr)/\1/' *.txt
 
 sed -i 's/ input_size//g' *.txt
@@ -29,4 +29,7 @@ sed -i 's/ makespan_static//g' *.txt
 sed -i 's/ makespan_dynamic//g' *.txt
 
 sed -i '1i algo_nr wf_name inp_size dur_alg1 ms_1 dur_alg2 ms_2 ' *.txt
-sed -i 's/  \+/ /g' *.txt
+sed -i 's/  \+/ /g' *.txt #replace multiple spaces with a single space
+sed -i '/^$/d' *.txt #remove empty lines
+sed -i '/^\([[:space:]]*\([^[:space:]]\+\([[:space:]]\+[^[:space:]]\+\)\{0,2\}\)[[:space:]]*\)$/d' *.txt #remove lines with less than 3 columns (malformed, experiments that did not finish)
+
